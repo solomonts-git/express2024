80f2bab8284794a27995d5c312ae65f1db5f311c
solomon <solomongorevachew@gmail.com>
2024-02-22 15:34:13 -0800

    using get method

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,21 @@ import express from "express";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+  // res.send({msg:"Hello, World!"});
+  // res.status(201).send({msg:"Hello, World!"});
+});
+
+app.get("/api/users", (req, res) => {
+  res.send([
+    { id: 1, username: "degu", displayName: "Degu" },
+    { id: 2, username: "tamerat", displayName: "Tamerat" },
+    { id: 3, username: "teddy", displayName: "Teddy" },
+  ]);
+});
+
 app.listen(PORT, () => {
   console.log(`Server start at ${PORT}`);
 });
