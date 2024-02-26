da2604f4c9dbdac703ffa1d5a534e8398886fcc0
solomon <solomongorevachew@gmail.com>
2024-02-26 10:06:17 -0800

    put, patch and delete method added

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+//middleware to parse request body
+app.use(express.json());
+
 const mockUsers = [
   { id: 1, username: "degu", displayName: "Degu" },
   { id: 2, username: "tamerat", displayName: "Tamerat" },
@@ -43,6 +46,57 @@ app.get("/api/users/:id", (req, res) => {
   if (!findUser) return res.sendStatus(404);
 
   return res.send(findUser);
+});
+
+app.post("api/users", (req, res) => {
+  // console.log(req.body);
+  const { body } = req.body;
+  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+  mockUsers.push(newUser);
+  return res.status(201).send(newUser);
+});
+// put -- used to update the entire request, if you miss
+// the data will be lost
+app.put("/api/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  const parsedId = parseInt(id);
+
+  if (isNaN(parsedId)) return res.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return res.sendStatus(404);
+  mockUsers[findUserIndex] = { id: parsedId, ...body };
+  return res.sendStatus(200);
+});
+
+// patch -- used to patch partially
+app.patch("/api/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+  const parsedId = parseInt(id);
+
+  if (isNaN(parsedId)) return res.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return res.sendStatus(404);
+
+  mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
+  return res.sendStatus(200);
+});
+
+app.delete("api/users/:id", (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return res.sendStatus(400);
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) return res.sendStatus(404);
+  mockUsers.splice(findUserIndex, 1);
+  return res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
